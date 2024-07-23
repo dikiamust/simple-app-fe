713b978d5188703826cbc6ef4cf0ec5e0d2379c2
dikiamust <agungmustaqimdiki@gmail.com>
2024-07-23 15:40:44 +0700

    feat: add calender commponent and consume agenda api

--- containers/Home/agenda/index.tsx
+++ containers/Home/agenda/index.tsx
@@ -10,6 +10,15 @@ import axios from 'axios';
 
 // base URL
 import BASE_URL from 'utils/baseUrl';
+import {
+  ScheduleComponent,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+} from '@syncfusion/ej2-react-schedule';
 
 const Section = styled(Box)(({ theme }) => ({
   padding: '0 80px',
@@ -36,16 +45,51 @@ const Title = styled(Typography)(({ theme }) => ({
   },
 }));
 
-const Agenda = () => {
+const Agendas = () => {
+  const [dataAgenda, setDataAgenda] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}agenda/public`, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Api-Version': 'v1',
+          },
+        });
+
+        const data = response.data.data.map((item: any) => ({
+          Id: item.id,
+          Subject: item.title,
+          StartTime: new Date(item.startDateTime),
+          EndTime: new Date(item.endDateTime),
+        }));
+        setDataAgenda(data);
+      } catch (error) {
+        console.error('Error fetching agenda data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Section>
         <Container>
           <Title>View All Events</Title>
+          <ScheduleComponent
+            selectedDate={new Date()}
+            eventSettings={{
+              dataSource: dataAgenda,
+            }}
+          >
+            <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+          </ScheduleComponent>
         </Container>
       </Section>
     </>
   );
 };
 
-export default Agenda;
+export default Agendas;
